1ed48d3105695bc4a55a4c15b13bd61ef4204c7d
HoDucAnh <84118304+hoducanh0232001@users.noreply.github.com>
2022-10-15 22:29:59 +0700

    post method

--- app/src/main/java/com/example/api_test/API/ApiService.java
+++ app/src/main/java/com/example/api_test/API/ApiService.java
@@ -1,21 +1,28 @@
 package com.example.api_test.API;
 
 import com.example.api_test.model.Currency;
+import com.example.api_test.model.Post;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface ApiService {
-    //link api : http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
-
+    //link GET api : http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
+    // link POST api: https://jsonplaceholder.typicode.com/posts
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 
-    ApiService apiService = new Retrofit.Builder().baseUrl("http://apilayer.net/")
+    ApiService apiService = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
@@ -26,4 +33,25 @@ public interface ApiService {
                                    @Query("currencies") String currencies,
                                    @Query("source") String source,
                                    @Query("format") int format);
+
+    @GET("api/live")
+    Call<Currency> convertUsdToVnd2(@QueryMap Map<String ,String> options);
+
+    //Link API : http://api.layer.net/api/users/list
+    @GET("api/users/list")
+    Call<Currency> getListUser();
+
+    //Link API : http://api.layer.net/api/group/1/users
+    @GET("api/group/{id}/users")
+    Call<Currency> getListUserFromGroup(@Path("id") int groupId);
+
+    //Link API : http://api.layer.net/api/group/1/users?sort=desc
+    @GET("api/group/{}/users")
+    Call<Currency> getListUserFromGroup2(@Path("id")int groupId,
+                                         @Query("sort") String sort);
+
+    @POST("posts")
+    Call<Post> sendPosts(@Body Post post);
+
+
 }
--- app/src/main/java/com/example/api_test/MainActivity.java
+++ app/src/main/java/com/example/api_test/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 
 import com.example.api_test.API.ApiService;
 import com.example.api_test.model.Currency;
+import com.example.api_test.model.Post;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,8 +21,8 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvTerms, tvSource, tvUSDVND;
-    private Button btnCallApi;
+    private TextView tvTerms, tvSource, tvUSDVND,tvTerms2, tvSource2, tvUSDVND2, tvPost;
+    private Button btnCallApi, btnCallApi2, btnPostApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +37,52 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 clickCallApi();
+            }
+        });
+
+        tvTerms2 = findViewById(R.id.tv_terms_2);
+        tvSource2 = findViewById(R.id.tv_source_2);
+        tvUSDVND2 = findViewById(R.id.tv_usdVnd_2);
+        btnCallApi2 = findViewById(R.id.btn_call_api_2);
+
+        btnCallApi2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickCallApi2();
+            }
+        });
+
+        tvPost = findViewById(R.id.post_api);
+        btnPostApi = findViewById(R.id.btn_post_api);
+        btnPostApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendPosts();
+            }
+        });
+    }
+
+    private void clickCallApi2() {
+        Map<String, String> options = new HashMap<>();
+        options.put("access_key","843d4d34ae72b3882e3db642c51e28e6");
+        options.put("currencies","VND");
+        options.put("source","USD");
+        options.put("format","1");
+        ApiService.apiService.convertUsdToVnd2(options).enqueue(new Callback<Currency>() {
+            @Override
+            public void onResponse(Call<Currency> call, Response<Currency> response) {
+                Toast.makeText(MainActivity.this, "Call API Success 2", Toast.LENGTH_SHORT).show();
+                Currency currency = response.body();
+                if (currency!= null && currency.isSuccess()){
+                    tvTerms2.setText(currency.getTerms());
+                    tvSource2.setText(currency.getSource());
+                    tvUSDVND2.setText(String.valueOf(currency.getQuotes().getUsdVnd()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Currency> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Call API Failed 2", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -56,6 +106,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<Currency> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "Call API Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void sendPosts(){
+        Post post = new Post(10,100,"hello", " Ducanhday");
+        ApiService.apiService.sendPosts(post).enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                Toast.makeText(MainActivity.this, "Post API Success", Toast.LENGTH_SHORT).show();
+
+                Post postResult = response.body();
+                if(postResult != null ){
+                    tvPost.setText(postResult.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Post API Failed", Toast.LENGTH_SHORT).show();
             }
         });
     }
